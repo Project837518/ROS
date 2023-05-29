d8f45c163fd7f412f7c7bc690aed6b6bcf2da0a6
Madiar <100179219+Project837518@users.noreply.github.com>
2023-05-30 01:07:24 +0600

    Update Check_V.py

--- Check_V.py
+++ Check_V.py
@@ -14,16 +14,14 @@ else:
 
 model = joblib.load(model_file)
 
-# Load the PE file and convert it to a grayscale image
+
 pe = pefile.PE('putty.exe')
 raw_bytes = bytes(pe.__data__)
 image = Image.frombytes('L', (32, 32), raw_bytes)
 
-# Preprocess the image
 normalized_image = np.array(image) / 255.0
 reshaped_image = normalized_image.reshape(1, 32, 32, 1)
 
-# Make predictions on the preprocessed image
 predictions = model.predict(reshaped_image)
 
 print(predictions)
